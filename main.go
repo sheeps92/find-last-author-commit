5ffb23d53ee8ff4f53da94f1afd62adc520bdf3c
unknown <sheeps1992@gmail.com>
2021-09-13 21:12:11 -0400

    More func

--- main.go
+++ main.go
@@ -3,19 +3,20 @@ package main
 import (
 	"log"
 
-	"github.com/google/go-github/github"
 	"github.com/sheeps92/find-last-author-commit/src"
 )
 
-func run() *github.Contributor {
+func run() string {
 	creds := src.Creds()
 
 	users := src.FindUsers("sheeps92", "find-last-author-commit", creds)
+	date := src.FindCommit("sheeps92", "find-last-author-commit", users, creds)
+	log.Println(date)
 	return users
 }
 
 func main() {
-	if err := run(); err != nil {
+	if err := run(); err != "" {
 		log.Fatal(err)
 	}
 }
--- src/findCommit.go
+++ src/findCommit.go
@@ -1,5 +1,22 @@
 package src
 
-func FindCommit(repo string, author []string) {
+import (
+	"context"
+	"log"
+	"time"
 
+	"github.com/google/go-github/github"
+)
+
+func FindCommit(owner string, repo string, author string, creds *github.Client) *time.Time {
+	commit, _, err := creds.Repositories.ListCommits(context.TODO(), owner, repo, &github.CommitsListOptions{Author: author, ListOptions: github.ListOptions{PerPage: 1}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var commitDate *time.Time
+	for _, c := range commit {
+		yeet := c.Commit
+		commitDate = yeet.Committer.Date
+	}
+	return commitDate
 }
